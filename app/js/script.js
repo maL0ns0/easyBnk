564ab8c530a49f897816b1e1eda6e441e63ef2c5
Maxwell Alexander <alexander.maxwell84@gmail.com>
2022-03-30 11:56:37 -0600

    Se termino con la animacion del hamburgerMenu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,21 @@
-console.log('hello world')
+let menu = document.querySelector('#btnHamburger')
+let header = document.querySelector('.header')
+let overlay = document.querySelector('.overlay')
+
+menu.addEventListener('click', () => {
+    console.log('Open Hamburguer')
+
+    if(header.classList.contains('open')){ 
+        //close menu
+        header.classList.remove('open')
+        overlay.classList.add('fade-out')
+        overlay.classList.remove('fade-in')
+        
+    }
+    else{ 
+        //open menu
+        header.classList.add('open')
+        overlay.classList.add('fade-in')
+        overlay.classList.remove('fade-out')
+    }
+})
