da45c469e735d032aa7df4bcb9b6823f8179bab4
Maxwell Alexander <alexander.maxwell84@gmail.com>
2022-03-30 15:52:22 -0600

    Terminamos animacion del menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,21 +1,29 @@
+let bdy = document.querySelector('body');
 let menu = document.querySelector('#btnHamburger')
 let header = document.querySelector('.header')
 let overlay = document.querySelector('.overlay')
+let fadeElems = document.querySelectorAll('.has-fade')
 
 menu.addEventListener('click', () => {
     console.log('Open Hamburguer')
 
     if(header.classList.contains('open')){ 
         //close menu
+        bdy.classList.remove('no-scroll');
         header.classList.remove('open')
-        overlay.classList.add('fade-out')
-        overlay.classList.remove('fade-in')
+        fadeElems.forEach((e)=> {
+            e.classList.add('fade-out')
+            e.classList.remove('fade-in')
+        })
         
     }
     else{ 
         //open menu
+        bdy.classList.add('no-scroll');
         header.classList.add('open')
-        overlay.classList.add('fade-in')
-        overlay.classList.remove('fade-out')
+        fadeElems.forEach((e)=> {
+            e.classList.remove('fade-out')
+            e.classList.add('fade-in')
+        })
     }
 })
